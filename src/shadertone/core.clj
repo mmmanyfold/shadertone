8f3ec4858037eb14655efa555da9ba121d014243
dviramontes <dviramontes@gmail.com>
2016-04-25 01:18:37 -0600

    sends shell command with keycombo on shader switch

--- src/shadertone/core.clj
+++ src/shadertone/core.clj
@@ -1,6 +1,7 @@
 (ns shadertone.core
   (:use [overtone.live])
-  (:require [shadertone.tone :as t])
+  (:require [shadertone.tone :as t]
+            [clojure.java.shell :refer [sh]])
   (:gen-class))
 
 ; Depending on your audio setup (external interfaces, etc...) you might need
@@ -17,6 +18,9 @@
 ;; for the repl
 ;;(def lowpass (inst-fx! external fx-rlpf))
 
+(defn send-shell-cmd [n]
+  (let [cmd (format "tell application \"System Events\" to keystroke \"%s\" using {command down}" n)]
+    (sh "osascript" "-e" cmd)))
 
 (defn refresh!
   "from the repl we can use this function to swap shaders on the fly
@@ -25,13 +29,17 @@
   no .glsl extension needed or directory, as you can see below it add
   it to the name."
   ([]
-   (t/start "examples/0.glsl"
-            :width 1440 :height 900
-            :user-data {"t0" (atom {:synth nil :tap "t0"})}))
+   (let []
+     (t/start "examples/0.glsl"
+              :width 1440 :height 900
+              :user-data {"t0" (atom {:synth nil :tap "t0"})})
+     (send-shell-cmd 0)))
   ([shader]
-   (t/start (str "examples/" shader ".glsl")
-            :width 1440 :height 900
-            :user-data {"t0" (atom {:synth nil :tap "t0"})})))
+   (let []
+     (t/start (str "examples/" shader ".glsl")
+              :width 1440 :height 900
+              :user-data {"t0" (atom {:synth nil :tap "t0"})})
+     (send-shell-cmd shader))))
 
 
 (defn midi-listen []
