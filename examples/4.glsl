6b1856c4124471a99b25f18a31b1696f43a21863
eemshi <oreneemshi@gmail.com>
2016-04-28 20:49:31 -0600

    make shader 4 awesome pt. 1

--- examples/4.glsl
+++ examples/4.glsl
@@ -4,11 +4,23 @@
 uniform float iOvertoneVolume;
 void main(void)
 {
-    vec2 uv = 2.0*(gl_FragCoord.xy/iResolution.xy) - 1.0;
+    vec2 uv = 3.0*(gl_FragCoord.xy/iResolution.xy) - 1.0;
+    vec2 uv2 = 3.0*(gl_FragCoord.xy/iResolution.xy) - 1.0;
+    vec2 uv3 = 3.0*(gl_FragCoord.xy/iResolution.xy) - 1.0;
+    vec2 uv4 = 4.0*(gl_FragCoord.xy/iResolution.xy) - 2.0;
     // equvalent to the video's spec.y, I think
-    float spec_y = 0.01 + 5.0*iOvertoneVolume;
-    float col = 0.0;
+    float spec_y = 0.1 + 5.0*iOvertoneVolume;
+    float col = 0.5;
+    float col2 = 0.09;
+    float col3 = 0.055;
+    float col4 = 0.02;
     uv.x += sin(iGlobalTime * 6.0 + uv.y*1.5)*spec_y;
+    uv2.x += sin(iGlobalTime * 6.0 + uv2.y*1.5)*spec_y;
+    uv3.x += sin(iGlobalTime * -6.0 + uv3.y*1.5)*spec_y;
+    uv4.x += sin(iGlobalTime * -6.0 + uv.y*2.5)*spec_y;
     col += abs(0.066/uv.x) * spec_y;
-    gl_FragColor = vec4(col,0.5,0.9,1.0);
+    col2 += abs(1.066/uv2.x) * spec_y;
+    col3 += abs(2.16/uv3.x) * spec_y;
+    col4 += abs(0.16/uv4.x) * spec_y;    
+    gl_FragColor = vec4(col,col2,col3,col4);
 }
